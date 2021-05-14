a46fd2613938b682fe499f0a5d21dc31dd724001
hasantha32 <hasanthamadushan32@gmail.com>
2021-05-13 19:59:35 -0800

    Finalized

--- LabSheet10.3/src/com/Item.java
+++ LabSheet10.3/src/com/Item.java
@@ -14,7 +14,7 @@ public class Item
 			Class.forName("com.mysql.jdbc.Driver");
 			
 			//Provide the correct details: DBServer/DBName, username, password
-			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/paf","root", "root");
+			con= DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/newdb", "root", "");
 		}
 		catch (Exception e)
 		{
